86abd27e9b35e54c16ce91f4dd2b2a3da645529a
zeav <arne@zeav.no>
2012-11-17 15:41:26 +0100

    first commit

---
+++ ProgTek-WebCrawler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgTek_WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
